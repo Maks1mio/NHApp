bdb795d3f1a8c856a127ef503db6c9868ba01b38
EvT <44835662+Maks1mio@users.noreply.github.com>
2025-06-14 02:37:23 +0300

    Complete removal of old submodule

--- src/main/renderer.ts
+++
@@ -1,3 +0,0 @@
-// This file is the entry point for the renderer process in Electron.
-import '../renderer/App'
-import '../../static/styles/globals.css'
--- src/renderer/types.ts
+++
@@ -1,6 +0,0 @@
-// This file is used to define the structure of the items in the tree
-// and the types of the props used in the components.
-
-export interface HeaderProps {
-  title: string;
-}
